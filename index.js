b01b99353cfee6849d8313ca9aa0f39d7b57014a
MohamedFahadTech <mohamedfahadur@gmail.com>
2025-12-24 15:49:33 +0530

    Add nodemon for development and update dependencies in package.json
    - Added "start" script to use nodemon for automatic server restarts.
    - Included "cors" and "install" as new dependencies.
    - Added "nodemon" as a devDependency for easier development.
    - Updated "npm" version in dependencies.

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({
+	origin: "http://localhost:5173",
+	credentials: true
+}));
+
 require("dotenv").config();
+const user = require("./models/user");
 
 connectDB();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
 	console.log(`Server running on http://localhost:${PORT}`)
 );
+
+app.post("/user/login", async (req, res) => {
+
+	const { email, password } = req.body;
+
+	const userExist = await user.findOne({ email: email });
+	console.log(userExist)
+
+	if (userExist) {
+
+		if (userExist.password === password) {
+			return res.json({ message: 'Login successful', success: true })
+		}
+		else {
+			return res.json({ message: 'Password does not match', success: false })
+		}
+	}
+	else {
+		return res.json({ message: 'User not found', success: false })
+	}
+});
